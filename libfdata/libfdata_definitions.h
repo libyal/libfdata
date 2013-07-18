4d578e10a93d55157b564e63e64d6026ab0a063f
Joachim Metz <joachim.metz@gmail.com>
2013-07-18 07:18:51 +0200

    Changes of version: 20130718

--- libfdata/libfdata_definitions.h
+++ libfdata/libfdata_definitions.h
@@ -33,11 +33,11 @@
  * for local use of libfdata
  */
 #else
-#define LIBFDATA_VERSION						20130616
+#define LIBFDATA_VERSION						20130718
 
 /* The libfdata version string
  */
-#define LIBFDATA_VERSION_STRING						"20130616"
+#define LIBFDATA_VERSION_STRING						"20130718"
 
 /* The library flag definitions
  */
