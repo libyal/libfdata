e2dfa3c502a43be682cea7bb6e81b4ca5846b417
Joachim Metz <joachim.metz@gmail.com>
2019-11-15 05:13:28 +0100

    Applied updates

--- .travis/script.sh
+++ .travis/script.sh
@@ -25,7 +25,7 @@ then
 
 		make install DESTDIR=${PWD}/osx-pkg;
 		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libfdata;
-		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libfdata;
+		cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libfdata;
 
 		pkgbuild --root osx-pkg --identifier com.github.libyal.libfdata --version ${VERSION} --ownership recommended ../libfdata-${VERSION}.pkg;
 	fi
--- acinclude.m4
+++ acinclude.m4
@@ -1,6 +1,6 @@
 dnl Functions for libfdata
 dnl
-dnl Version: 20190308
+dnl Version: 20190811
 
 dnl Function to detect if libfdata dependencies are available
 AC_DEFUN([AX_LIBFDATA_CHECK_LOCAL],
